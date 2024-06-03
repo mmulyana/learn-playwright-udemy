78c60af43184269b9679f61ce81d9e7c39012ecd
mmulyana <mulyan.t20@gmail.com>
2024-06-04 01:00:36 +0700

    part 8

---
+++ tests/calendar.spec.js
@@ -0,0 +1,29 @@
+const { test } = require('@playwright/test')
+
+const expected = ['']
+
+test('Calendar validation', async ({ page }) => {
+  const date = '15'
+  const year = '2027'
+  const month = '6'
+  const expectedList = [month, date, year]
+
+  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers')
+
+  await page.locator('.react-date-picker__inputGroup').click()
+  await page.locator('.react-calendar__navigation__label').click()
+  await page.locator('.react-calendar__navigation__label').click()
+
+  await page.getByText(year).click()
+  await page
+    .locator('.react-calendar__year-view__months__month')
+    .nth(Number(month) - 1)
+    .click()
+  await page.locator("//abbr[text()='" + date + "']").click()
+
+  const inputs = await page.locator('.react-date-picker__inputGroup input')
+  for (let i = 0; i < inputs.length; i++) {
+    const value = inputs[i].getAttribute('value')
+    expect(value).toEqual(expectedList[i])
+  }
+})
